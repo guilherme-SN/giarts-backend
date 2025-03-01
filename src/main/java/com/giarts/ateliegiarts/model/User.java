3828f7a16645fe7d30eba02c0cb30acf53031185
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2025-03-01 12:07:00 -0300

    refactor: reorganize lombok and import of user

--- src/main/java/com/giarts/ateliegiarts/model/User.java
+++ src/main/java/com/giarts/ateliegiarts/model/User.java
@@ -1,15 +1,17 @@
 package com.giarts.ateliegiarts.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
-@Getter
-@Setter
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/giarts/ateliegiarts/service/UserService.java
+++ src/main/java/com/giarts/ateliegiarts/service/UserService.java
@@ -14,7 +14,10 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
